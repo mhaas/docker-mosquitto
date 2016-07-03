474fb9bef358c038594dd615306e7def25955898
Michael Haas <haas@computerlinguist.org>
2016-07-03 20:36:10 +0200

    run.sh: Set restart to 'unless-stopped'

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # --restart=always enables autostart on boot:
 # https://serverfault.com/questions/633067/how-do-i-auto-start-docker-containers-at-system-boot
-docker run --detach=true --name=mqtt --restart=always -p 1883:1883 mhaas/mosquitto
+docker run --volumes-from mqtt-data --detach=true --name=mqtt --restart=unless-stopped -p 1883:1883 mhaas/mosquitto

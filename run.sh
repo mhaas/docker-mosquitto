526e21d4d0d08299140ef23a862663e2fd9446e4
Michael Haas <haas@computerlinguist.og>
2018-01-03 12:40:33 +0100

    Switch run.sh to named volumes
    You have to create these first.

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 # --restart=always enables autostart on boot:
 # https://serverfault.com/questions/633067/how-do-i-auto-start-docker-containers-at-system-boot
-docker run --volumes-from mqtt-data --detach=true --name=mqtt --restart=unless-stopped -p 1883:1883 mhaas/mosquitto
+# Create named volumes first:
+#docker volume create mosquitto-data
+#docker volume create mosquitto-logs
+#docker volume create mosquitto-config
+docker run \
+    -v mosquitto-data:/var/lib/mosquitto \
+    -v mosquitto-logs:/var/log/mosquitto \
+    -v mosquitto-config:/etc/mosquitto \
+    --detach=true \
+    --name=mosquitto \
+    --restart=unless-stopped \
+    -p 1883:1883 \
+    mhaas/mosquitto
